9fd23651dc54c65cd0fa17ebe64569e096266f15
Hadeeb Farhan <hadeebfarhan1@gmail.com>
2018-07-14 18:06:42 +0530

    Initial Setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: "../examples/index.html",
+  filename: "index.html",
+  inject: "body"
+});
+module.exports = {
+  context: __dirname + "/examples",
+  entry: "./index.js",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/examples/build"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+};
