98b52c97f01e00e7f2f78cfed89cfaee47f01286
Hadeeb Farhan <hadeebfarhan1@gmail.com>
2018-07-15 00:10:01 +0530

    Added npm install script

--- test/Donut.test.js
+++ test/Donut.test.js
@@ -3,7 +3,12 @@ import renderer from "react-test-renderer";
 import Donut from "../lib/Donut";
 import props from "./mock";
 
-it("renders correctly", () => {
+it("renders correctly with props", () => {
   const tree = renderer.create(<Donut {...props} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders with warnings without props", () => {
+  const tree = renderer.create(<Donut />).toJSON();
   expect(tree).toMatchSnapshot();
 });
